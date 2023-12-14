25b5665c59646d141eb99510048f5862467ce20b
BenjaIGVM <115327548+BenjaIGVM@users.noreply.github.com>
2023-12-14 21:55:01 +0100

    Update auth.js

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -2,10 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const auth = (value) => {
   try {
-    // Verificar la validez del token y decodificarlo
-    const decoded = jwt.verify(value, 'SUPER_HYPER_MEGA_PALABRA_SECRETA'); // Reemplaza 'clave-secreta' con tu clave secreta real
-
-    // Devolver los datos del usuario extraídos del token
+    const decoded = jwt.verify(value, 'SUPER_HYPER_MEGA_PALABRA_SECRETA'); 
     return decoded.usuario;
   } catch (error) {
     throw new Error('Token inválido o expirado');
